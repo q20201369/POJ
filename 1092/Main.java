ad674eea42bb0a107f84b529709ec4ce857fd7af
q20201369 <NA>
2017-12-22 17:08:02 +0800

    Add IO code

--- 1092/Main.java
+++ 1092/Main.java
@@ -1,7 +1,54 @@
+import java.util.Scanner;
+import java.util.Vector;
 
 public class Main
 {
+	public static int findRegions(Vector<Vertex> vertices, int regionSize)
+	{
+		return 0;
+	}
+
 	public static void main(String[] args)
 	{
+		Scanner sc = new Scanner(System.in);
+		int testCases = sc.nextInt();
+
+		for (int i = 0; i < testCases; ++i)
+		{
+			Vector<Vertex> vertices = new Vector<Vertex>();
+
+			int verticesCount = sc.nextInt();
+			String line = sc.nextLine(); // consume the next line separator
+			for (int v = 0; v < verticesCount; ++v)
+			{
+				line = sc.nextLine();
+				Scanner lineScanner = new Scanner(line);
+				Vertex vertex = new Vertex();
+				vertex.id = lineScanner.nextInt();
+				vertex.posX = lineScanner.nextInt();
+				vertex.posY = lineScanner.nextInt();
+				while (lineScanner.hasNextInt())
+				{
+					int neighbor = lineScanner.nextInt();
+					vertex.neighbor.add(neighbor);
+				}
+
+				vertices.add(vertex);
+			}
+
+			int regionSize = sc.nextInt();
+
+			int regions = findRegions(vertices, regionSize);
+			System.out.println(regions);
+		}
 	}
 }
+
+class Vertex
+{
+	public int id;
+	public int posX;
+	public int posY;
+	public Vector<Integer> neighbor = new Vector<Integer>();
+}
+
