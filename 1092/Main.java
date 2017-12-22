65381546acdf4aa03e988613b978e58eee31281c
q20201369 <NA>
2017-12-22 18:17:37 +0800

    Implement findRegions partially

--- 1092/Main.java
+++ 1092/Main.java
@@ -3,8 +3,48 @@ import java.util.Vector;
 
 public class Main
 {
-	public static int findRegions(Vector<Vertex> vertices, int regionSize)
+	private static void findRegion(Vector<Vertex> vertices, Vector<Vector<Integer>> regions, int startVertex, int regionSize, Vector<Integer> tempRegion)
 	{
+		int firstVertex = tempRegion.get(0);
+		int lastVertex = tempRegion.get(tempRegion.size() - 1);
+
+		if (tempRegion.size() >= regionSize)
+		{
+			// test if the first vertex in tempRegion is connected to the last vertex, if yes, the region is good for length
+			for (int i = 0; i < tempRegion.size(); ++i)
+			{
+				System.out.print(i + ", ");
+			}
+			System.out.println(";");
+
+			regions.add((Vector<Integer>)tempRegion.clone());
+			return;
+		}
+
+		Vector<Integer> lastVertexNeighbor = vertices.get(lastVertex).neighbor;
+		for (int i = 0; i < lastVertexNeighbor.size(); ++i)
+		{
+			int nextVertex = lastVertexNeighbor.get(i);
+			tempRegion.add(nextVertex);
+			findRegion(vertices, regions, startVertex, regionSize, tempRegion);
+			tempRegion.remove(tempRegion.size() - 1);
+		}
+	}
+
+	private static int findRegions(Vector<Vertex> vertices, int regionSize)
+	{
+		if (vertices.size() <= 2)
+			return 0;
+
+		Vector<Vector<Integer>> regions = new Vector<Vector<Integer>>();
+		Vector<Integer> tempRegion = new Vector<Integer>();
+		for (int i = 0; i < vertices.size(); ++i)
+		{
+			tempRegion.clear();
+			tempRegion.add(i);
+			findRegion(vertices, regions, i, regionSize, tempRegion);
+		}
+
 		return 0;
 	}
 
