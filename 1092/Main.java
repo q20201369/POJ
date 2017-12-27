e1c932fc014c5e4d6ef3d76b52a9cb102891a838
q20201369 <NA>
2017-12-28 07:25:37 +0800

    Improved 1092 to find regions w/ the right size, but still need to remove the regions w/ vertices inside

--- 1092/Main.java
+++ 1092/Main.java
@@ -21,13 +21,36 @@ public class Main
 		return false;
 	}
 
+	private static double getAngle(int basePosX, int basePosY, int newPosX, int newPosY)
+	{
+		int shiftedPosX = newPosX - basePosX;
+		int shiftedPosY = newPosY - basePosY;
+
+		if (shiftedPosX == 0)
+		{
+			if (shiftedPosY > 0)
+				return Math.PI / 2;
+			return - Math.PI / 2;
+		}
+
+		return Math.atan2(shiftedPosY, shiftedPosX);
+	}
+
 	private static void findRegion(Vector<Vertex> vertices, Vector<Vector<Integer>> regions, int startVertex, int regionSize, Vector<Integer> tempRegion)
 	{
 		int firstVertex = tempRegion.get(0);
+		int secondVertex = tempRegion.get(1);
 		int lastVertex = tempRegion.get(tempRegion.size() - 1);
+		int lastLastVertex = tempRegion.get(tempRegion.size() - 2);
 
 		if (tempRegion.size() >= regionSize)
 		{
+			for (int i = 0; i < tempRegion.size(); ++i)
+			{
+				System.out.print(tempRegion.get(i) + ", ");
+			}
+			System.out.println("#");
+
 			if (!areTwoVerticesConnected(vertices, firstVertex, lastVertex))
 				return;
 
@@ -43,15 +66,41 @@ public class Main
 			regions.add((Vector<Integer>)tempRegion.clone());
 			return;
 		}
+		for (int i = 0; i < tempRegion.size(); ++i)
+		{
+			System.out.print(tempRegion.get(i) + ", ");
+		}
+		System.out.println("*");
 
 		Vector<Integer> lastVertexNeighbor = vertices.get(lastVertex).neighbor;
+		// find a vertex so that it has smallest angle between last segment and the new segment
+		int bestVertex = -1;
+		double angle = getAngle(vertices.get(lastVertex).posX, vertices.get(lastVertex).posY, vertices.get(lastLastVertex).posX, vertices.get(lastLastVertex).posY);
+		double bestAngle = 100000000;
 		for (int i = 0; i < lastVertexNeighbor.size(); ++i)
 		{
 			int nextVertex = lastVertexNeighbor.get(i);
-			tempRegion.add(nextVertex);
-			findRegion(vertices, regions, startVertex, regionSize, tempRegion);
-			tempRegion.remove(tempRegion.size() - 1);
+			if (bestVertex < 0)
+				bestVertex = nextVertex;
+
+			if (nextVertex == lastLastVertex)
+				continue;
+
+			System.out.println("last " + lastVertex + " neighbor " + nextVertex);
+
+			double nextAngle = getAngle(vertices.get(lastVertex).posX, vertices.get(lastVertex).posY, vertices.get(nextVertex).posX, vertices.get(nextVertex).posY);
+			double deltaAngle = Math.abs((angle - nextAngle) % (2*Math.PI));
+			System.out.println("nextAngle: " + nextAngle + " angle: " + angle + " delta: " + deltaAngle);
+			if (deltaAngle < bestAngle)
+			{
+				bestAngle = deltaAngle;
+				bestVertex = nextVertex;
+			}
 		}
+
+		tempRegion.add(bestVertex);
+		findRegion(vertices, regions, startVertex, regionSize, tempRegion);
+		tempRegion.remove(tempRegion.size() - 1);
 	}
 
 	private static int findRegions(Vector<Vertex> vertices, int regionSize)
@@ -65,10 +114,20 @@ public class Main
 		{
 			tempRegion.clear();
 			tempRegion.add(i);
-			findRegion(vertices, regions, i, regionSize, tempRegion);
+
+			Vector<Integer> vertexNeighbor = vertices.get(i).neighbor;
+			for (int j = 0; j < vertexNeighbor.size(); ++j)
+			{
+				int nextVertex = vertexNeighbor.get(j);
+				if (nextVertex == i)
+					continue;
+				tempRegion.add(nextVertex);
+				findRegion(vertices, regions, i, regionSize, tempRegion);
+				tempRegion.remove(tempRegion.size() - 1);
+			}
 		}
 
-		return 0;
+		return regions.size();
 	}
 
 	public static void main(String[] args)
