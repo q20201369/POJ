9aa92233991d5a469788b81edcd535daf0f21fcc
q20201369 <NA>
2018-09-10 22:48:10 +0800

    Initial of p1013

---
+++ 1013/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int cases = sc.nextInt();
+		for (int i = 0; i < cases; ++i)
+		{
+			for (int j = 0; j < 3; ++j) {
+				String leftCoins = sc.next();
+				String rightCoins = sc.next();
+				String weigh = sc.next();
+			}
+
+			System.out.println("K is the counterfeit coin and it is light.");
+		}
+	}
+}
