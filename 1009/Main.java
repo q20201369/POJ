d9d6890725196fd6666f0a89d218e1d98e7ca4c4
q20201369 <NA>
2018-09-13 21:25:38 +0800

    p1009: generates edge rows

--- 1009/Main.java
+++ 1009/Main.java
@@ -3,6 +3,7 @@ import java.util.Vector;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Hashtable;
+import java.util.Arrays;
 
 public class Main
 {
@@ -17,14 +18,141 @@ public class Main
 				break;
 
 			Image image = parseImage(sc, width);
-			Image edgedImage = image.getEdgedImage();
-			LinkedList<RunLengthEncoding> encodedImage = edgedImage.toRunLengthEncoding();
+			LinkedList<ImageRow> rowBuffer = new LinkedList<ImageRow>();
 
+			ImageRow tempRow = new ImageRow(width);
+			int tmpRowSize = 0;
+
+			while (image.rleData.size() > 0)
+			{
+				RunLengthEncoding data = image.rleData.removeFirst();
+				if (data.length + tmpRowSize < width)
+				{
+					for (int i = tmpRowSize; i < tmpRowSize + data.length; ++i)
+						tempRow.pixels[i] = data.pixel;
+
+					tmpRowSize += data.length;
+				}
+				else
+				{
+					int pixelsLeftInCurrentRow = width - tmpRowSize;
+					for (int i = tmpRowSize; i < width; ++i)
+					{
+						tempRow.pixels[i] = data.pixel;
+					}
+					rowBuffer.addLast(tempRow);
+					tempRow = new ImageRow(width);
+					tmpRowSize = 0;
+
+					int leftPixelLength = data.length - pixelsLeftInCurrentRow;
+					int pureRowsLeft = leftPixelLength / width;
+					int pixelLengthAfterPureRows = leftPixelLength - pureRowsLeft * width;
+
+					if (pureRowsLeft == 0)
+					{
+						tempRow = new ImageRow(width);
+						tmpRowSize = pixelLengthAfterPureRows;
+						for (int i = 0; i < tmpRowSize; ++i)
+							tempRow.pixels[i] = data.pixel;
+					}
+					else if (pureRowsLeft < 3)
+					{
+						for (int i = 0; i < pureRowsLeft; ++i)
+						{
+							ImageRow pureRow = new ImageRow(width);
+							Arrays.fill(pureRow.pixels, data.pixel);
+							rowBuffer.addLast(pureRow);
+						}
+
+						tempRow = new ImageRow(width);
+						tmpRowSize = pixelLengthAfterPureRows;
+						for (int i = 0; i < tmpRowSize; ++i)
+							tempRow.pixels[i] = data.pixel;
+					}
+					else
+					{
+						ImageRow pureRow = new ImageRow(width);
+						Arrays.fill(pureRow.pixels, data.pixel);
+						rowBuffer.addLast(pureRow);
+
+						pureRow = new ImageRow(width, pureRowsLeft - 2);
+						Arrays.fill(pureRow.pixels, data.pixel);
+						rowBuffer.addLast(pureRow);
+
+						pureRow = new ImageRow(width);
+						Arrays.fill(pureRow.pixels, data.pixel);
+						rowBuffer.addLast(pureRow);
+
+						tempRow = new ImageRow(width);
+						tmpRowSize = pixelLengthAfterPureRows;
+						for (int i = 0; i < tmpRowSize; ++i)
+							tempRow.pixels[i] = data.pixel;
+					}
+				}
+			}
+
+			System.out.println(width);
+
+			for (int i = 0; i < rowBuffer.size(); ++i)
+			{
+				System.out.println(rowBuffer.get(i).toString());
+			}
+
+			// calculate edge
+			LinkedList<ImageRow> edgeRows = new LinkedList<ImageRow>();
+			for (int i = 0; i < rowBuffer.size(); ++i)
+			{
+				ImageRow row = new ImageRow(width, rowBuffer.get(i).count);
+				for (int j = 0; j < width; ++j)
+				{
+					int[] maxDelta = new int[1];
+
+					if (j > 0)
+					{
+						if (i > 0)
+							testMaxDelta(rowBuffer, i-1, j-1, i, j, maxDelta);
+
+						testMaxDelta(rowBuffer, i, j-1, i, j, maxDelta);
+
+						if (i < rowBuffer.size() - 1)
+							testMaxDelta(rowBuffer, i+1, j-1, i, j, maxDelta);
+					}
+
+					if (i > 0)
+						testMaxDelta(rowBuffer, i-1, j, i, j, maxDelta);
+
+					if (i < rowBuffer.size() - 1)
+						testMaxDelta(rowBuffer, i+1, j, i, j, maxDelta);
+
+					if (j < width-1)
+					{
+						if (i > 0)
+							testMaxDelta(rowBuffer, i-1, j+1, i, j, maxDelta);
+
+						testMaxDelta(rowBuffer, i, j+1, i, j, maxDelta);
+
+						if (i < rowBuffer.size() - 1)
+							testMaxDelta(rowBuffer, i+1, j+1, i, j, maxDelta);
+					}
+
+					row.pixels[j] = maxDelta[0];
+				}
+
+				edgeRows.addLast(row);
+			}
+
+			for (int i = 0; i < edgeRows.size(); ++i)
+			{
+				System.out.println(edgeRows.get(i).toString());
+			}
+
+			/*
 			System.out.println(edgedImage.width);
 			for (int i = 0; i < encodedImage.size(); ++i)
 			{
 				System.out.println(encodedImage.get(i).pixel + " " + encodedImage.get(i).length);
 			}
+			*/
 			System.out.println("0 0");
 		}
 		System.out.println("0");
@@ -41,10 +169,17 @@ public class Main
 			if (pixel == 0 && length == 0)
 				break;
 
-			image.addPixels(new RunLengthEncoding(pixel, length));
+			image.addRunLengthEncodingData(new RunLengthEncoding(pixel, length));
 		}
 
 		return image;
+	}
+
+	private static void testMaxDelta(LinkedList<ImageRow> rowBuffer, int row, int column, int baseRow, int baseColumn, int[] maxDelta)
+	{
+		int delta = Math.abs(rowBuffer.get(row).pixels[column] - rowBuffer.get(baseRow).pixels[baseColumn]);
+		if (delta > maxDelta[0])
+			maxDelta[0] = delta;
 	}
 }
 
@@ -56,8 +191,31 @@ class ImageRow
 		this.count = count;
 	}
 
+	// FIXME: shall we call the first constructor instead?
+	public ImageRow(int width)
+	{
+		this.pixels = new int[width];
+		this.count = 1;
+	}
+
 	public int[] pixels;
 	public int count;
+
+	public String toString()
+	{
+		String str = new String();
+		for (int i = 0; i < this.pixels.length; ++i)
+		{
+			str += this.pixels[i] + ", ";
+		}
+
+		if (this.count > 1)
+		{
+			str += "@" + this.count;
+		}
+
+		return str;
+	}
 }
 
 class RunLengthEncoding
@@ -74,8 +232,7 @@ class RunLengthEncoding
 
 class Image
 {
-	public LinkedList<ImageRow> imageRows = new LinkedList<ImageRow>();
-	public int lastRowRemainingPixels = 0;
+	public LinkedList<RunLengthEncoding> rleData = new LinkedList<RunLengthEncoding>();
 	public int width = 0;
 
 	public Image(int width)
@@ -83,7 +240,7 @@ class Image
 		this.width = width;
 	}
 
-	public void addPixels(RunLengthEncoding data)
+	public void addRunLengthEncodingData(RunLengthEncoding data)
 	{
 		int pixel = data.pixel;
 		int length = data.length;
@@ -91,6 +248,11 @@ class Image
 		if (length <= 0)
 			return;
 
+		this.rleData.addLast(data);
+
+	}
+
+		/*
 		int remainingLength = length;
 
 		if (remainingLength <= this.lastRowRemainingPixels)
@@ -135,9 +297,10 @@ class Image
 			this.addPixels(new RunLengthEncoding(pixel, secondPart));
 			return;
 		}
+		*/
 
-	}
 
+	/*
 	public Image getEdgedImage()
 	{
 		Image edgedImage = new Image(this.width);
@@ -166,6 +329,7 @@ class Image
 		LinkedList<RunLengthEncoding> encoded = new LinkedList<RunLengthEncoding>();
 		return encoded;
 	}
+	*/
 }
 
 
