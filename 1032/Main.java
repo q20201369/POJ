f7a0408de581b77a87849af51d8a1c8b5624bece
q20201369 <NA>
2018-09-21 18:04:23 +0800

    p1032: accepted; also fixed one test case

--- 1032/Main.java
+++ 1032/Main.java
@@ -19,45 +19,39 @@ public class Main
 
 		int n = sc.nextInt();
 
-		int numberOfThrees = n / 3;
-		int numberOfTwos = 0;
-		int numberOfFours = 0;
-		int remainder = n % 3;
-		if (remainder == 1)
+		int finalGroups = 0;
+		int remainder = 0;
+		for (int groups = 2; groups <= n-1; ++groups)
 		{
-			numberOfFours = 1;
-			numberOfThrees--;
-		}
-		else if (remainder == 2)
-		{
-			numberOfTwos = 1;
+			// calculate the sum of groups 2,3,4,5
+			int sumStartsFromTwo = (2 + groups - 1 + 2) * groups / 2;
+			int sumStartsFromThree = (3 + groups - 1 + 3) * groups / 2;
+			int sumStartsFromTwoNextGroup = (2 + (groups+1) - 1 + 2) * (groups+1) / 2;
+
+			if (sumStartsFromTwo <= n && n < sumStartsFromTwoNextGroup)
+			{
+				finalGroups = groups;
+				remainder = n - sumStartsFromTwo;
+				break;
+			}
 		}
 
-		if (numberOfTwos == 1)
+		int[] groupSizes = new int[finalGroups];
+		for (int i = 0; i < groupSizes.length; ++i)
 		{
-			System.out.print(2);
-			for (int i = 0; i < numberOfThrees; ++i)
-			{
-				System.out.print(" " + 3);
-			}
-		}
-		else if (numberOfFours == 1)
-		{
-			for (int i = 0; i < numberOfThrees; ++i)
-			{
-				System.out.print(3 + " ");
-			}
-			System.out.print(4);
-		}
-		else
-		{
-			for (int i = 0; i < numberOfThrees-1; ++i)
-			{
-				System.out.print(3 + " ");
-			}
-			System.out.print(3);
+			groupSizes[i] = 2 + i;
 		}
 
+		int increaseIndex = groupSizes.length - 1;
+		for (int i = 0; i < remainder; ++i)
+		{
+			groupSizes[increaseIndex]++;
+			increaseIndex = (increaseIndex-1 + groupSizes.length) % groupSizes.length;
+		}
+
+		System.out.print(groupSizes[0]);
+		for (int i = 1; i < groupSizes.length; ++i)
+			System.out.print(" " + groupSizes[i]);
 		System.out.println("");
 	}
 }
